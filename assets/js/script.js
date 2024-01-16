a6d1eed2866640df3f443d4474db4225d7e48084
Md. Anower Hossan <anowerhossan1988@gmail.com>
2024-01-16 20:51:37 +0600

    cursor add

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,18 @@
 var okbtn = document.querySelector(".onLoad_body .icon");
 var onLoadMain = document.querySelector(".onLoad");
 var stopprelod = document.querySelector(".onLoad_body");
-console.log(stopprelod)
-$(window).on('load', function(){
-  setTimeout(function(){
+console.log(stopprelod);
+$(window).on("load", function () {
+  setTimeout(function () {
     stopprelod.classList.add("onLoad_disable");
-  }, 2000)
-    setTimeout(function(){
-        okbtn.classList.add("ok_bdb");
-    }, 2000)
-    okbtn.addEventListener('click', function(){
-        onLoadMain.classList.add('onLoadNone')
-    })
-})
-
+  }, 2000);
+  setTimeout(function () {
+    okbtn.classList.add("ok_bdb");
+  }, 2000);
+  okbtn.addEventListener("click", function () {
+    onLoadMain.classList.add("onLoadNone");
+  });
+});
 
 // main slider
 let slideshowElements = document.querySelectorAll(".slider_box");
@@ -31,8 +30,6 @@ setInterval(() => {
     curentElement.nextElementSibling.classList.add("current");
   }
 }, 4000);
-
-
 
 // Cursor
 
@@ -83,18 +80,17 @@ $(document).ready(function () {
 
 // const cursor = document.querySelector(".cursor");
 // document.addEventListener("mousemove", (e) => {
-//   // Calculate the cursor position taking into account the scroll position
 //   const adjustedX = e.pageX - cursor.clientWidth / 2;
 //   const adjustedY = e.pageY - cursor.clientHeight / 2;
 
-//   cursor.style.top = `${Math.min(
-//     window.innerHeight - cursor.clientHeight,
-//     Math.max(0, adjustedY)
-//   )}px`;
-//   cursor.style.left = `${Math.min(
-//     window.innerWidth - cursor.clientWidth,
-//     Math.max(0, adjustedX)
-//   )}px`;
+//   const maxX = window.innerWidth - cursor.clientWidth;
+//   const maxY = window.innerHeight - cursor.clientHeight;
+
+//   const clampedX = Math.min(maxX, Math.max(0, adjustedX));
+//   const clampedY = Math.min(maxY, Math.max(0, adjustedY));
+
+//   cursor.style.top = `${clampedY}px`;
+//   cursor.style.left = `${clampedX}px`;
 
 //   if (
 //     e.pageX >= 0 &&
@@ -103,16 +99,23 @@ $(document).ready(function () {
 //     e.pageY <= window.innerHeight
 //   ) {
 //     cursor.style.display = "block";
+
+//     // Check if cursor is close to the right edge
+//     const scrollbarWidth =
+//       window.innerWidth - document.documentElement.clientWidth;
+//     if (e.pageX > window.innerWidth - scrollbarWidth - 10) {
+//       cursor.style.display = "none";
+//     } else {
+//       cursor.style.display = "block"; // Ensure the cursor is displayed when not close to the right edge
+//     }
 //   } else {
 //     cursor.style.display = "none";
 //   }
 // });
+
 // document.addEventListener("mouseout", () => {
 //   cursor.style.display = "none";
 // });
-
-
-
 
 // Pass word vissible
 function viewPass() {
