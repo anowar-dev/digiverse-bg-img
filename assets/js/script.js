0e3c898711809906f798d477130c0c4682fb3437
Md. Anower Hossan <anowerhossan1988@gmail.com>
2024-01-28 22:39:11 +0600

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,31 @@ var onLoadMain = document.querySelector(".onLoad");
 var stopprelod = document.querySelector(".onLoad_body");
 
 $(window).on("load", function () {
-  setTimeout(function () {
-    stopprelod.classList.add("onLoad_disable");
-  }, 2000);
-  setTimeout(function () {
-    okbtn.classList.add("ok_bdb");
-  }, 2000);
+  // setTimeout(function () {
+  //   stopprelod.classList.add("onLoad_disable");
+  // }, 2000);
+  // setTimeout(function () {
+  //   okbtn.classList.add("ok_bdb");
+  // }, 2000);
   okbtn.addEventListener("click", function () {
     onLoadMain.classList.add("onLoadNone");
   });
 });
 
 // main slider
-let slideshowElements = document.querySelectorAll(".slider_box");
-let count = 1;
-setInterval(() => {
-  count++;
-  let curentElement = document.querySelector(".current");
-  curentElement.classList.remove("current");
-  if (count > slideshowElements.length) {
-    slideshowElements[0].classList.add("current");
-    count = 1;
-  } else {
-    curentElement.nextElementSibling.classList.add("current");
-  }
-}, 4000);
+// let slideshowElements = document.querySelectorAll(".slider_box");
+// let count = 1;
+// setInterval(() => {
+//   count++;
+//   let curentElement = document.querySelector(".current");
+//   curentElement.classList.remove("current");
+//   if (count > slideshowElements.length) {
+//     slideshowElements[0].classList.add("current");
+//     count = 1;
+//   } else {
+//     curentElement.nextElementSibling.classList.add("current");
+//   }
+// }, 4000);
 
 // ===========Custom Cursor start here =============
 $(document).ready(function () {
